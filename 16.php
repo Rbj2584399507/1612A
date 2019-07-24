4ab0de34a31ab2609ab300840d373a29fcd82ccb
1612A <2584399507@qq.com>
2019-07-24 08:40:37 +0800

    aaa

---
+++ 16.php
@@ -0,0 +1,16 @@
+<?php  
+
+echo day16();
+function day16(){
+
+	$target = 20;
+	 $array=[[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]];
+	 if(in_array($target, $array)){
+	 	echo 'false';
+	 }else{
+	 	echo 'true';
+	 }
+}
+
+
+?>
